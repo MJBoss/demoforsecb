34df362c875d90da0d3f73a4eb154ee0b63b6884
MJBoss <bustamantemichaeljohn@gmail.com>
2024-08-21 14:37:22 +0800

    I have added product class

--- src/demoappforc/DemoAppforC.java
+++ src/demoappforc/DemoAppforC.java
@@ -6,13 +6,22 @@ public class DemoAppforC {
 
  
     public static void main(String[] args) {
-        Grades gr = new Grades();
-        gr.addGrades(1011, "Mike", 1.2, 3.2, 1.2, 2.1);
-        gr.viewGrades();
-        
-        Grades gr1 = new Grades();
-        gr1.addGrades(1011, "John", 1.2, 3.2, 1.2, 2.1);
-        gr1.viewGrades();
+//        Grades gr = new Grades();
+//        gr.addGrades(1011, "Mike", 1.2, 3.2, 1.2, 2.1);
+//        gr.viewGrades();
+//        
+//        Grades gr1 = new Grades();
+//        gr1.addGrades(1011, "John", 1.2, 3.2, 1.2, 2.1);
+//        gr1.viewGrades();
+            
+            Product pr = new Product();
+            pr.addProduct(10011, "Soap", 20, 30, 20.00);
+            pr.viewProduct();
+            
+            Product pr1 = new Product();
+            pr1.addProduct(10012, "Cards", 0, 60, 50.00);
+            pr1.viewProduct();
+
     }
     
 }
